6dea73e0c1372be81b2e60c779c4b0870060aaf7
omkargb <omkargb107@gmail.com>
2021-07-30 16:45:09 +0530

    [omkargb] Add : Program files uploaded and displayed welcome message

---
+++ AddressBookProgram/AddressBookMain.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookProgram
+{
+    class AddressBookMain
+    {
+        public void Display()
+        {
+            Console.WriteLine(" Welcome to Address Book Program ");
+        }
+    }
+}
