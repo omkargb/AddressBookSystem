5a44d904b8d6b04594457c3a7681a4dade95cfa3
omkargb <omkargb107@gmail.com>
2021-07-31 18:50:27 +0530

    [omkargb] Refactor : Edit contact details implemented

--- AddressBookProgram/AddressBookMain.cs
+++ AddressBookProgram/AddressBookMain.cs
@@ -8,6 +8,46 @@ namespace AddressBookProgram
     {
         public static List<Person> ContactList = new List<Person>();
 
+        //starting application
+        public void Book()
+        {
+            Console.WriteLine(" Welcome to Address Book Program \n");
+            Console.WriteLine(" No contacts in address book... \n [ ADD CONTACT ] Please provide following details : \n");
+            AddPersonInfo();
+        }
+
+        //options to select operation
+        public static void Operations()
+        {
+            Console.WriteLine(" Available options : \t 1.Edit_contact\t\t 0.Exit ");
+            Console.Write(" Provide option :  ");
+            int check = int.Parse(Console.ReadLine());
+               switch(check)
+                {
+                case 1:
+                        Console.Write(" Enter Firstname to find and edit info : ");
+                        string findName = Console.ReadLine();
+                        foreach (var entry in ContactList)
+                        {
+                            if (entry.FirstName.ToLower() == findName.ToLower())
+                            {
+                                    ModifyPersonInfo();
+                                    DisplayContacts();
+                            }
+                            else
+                            {
+                                Console.WriteLine(" Firstname does not match. Please retry. \n");
+                                Operations();
+                            }
+                        }
+                        Operations();
+                        break;
+                default:
+                        break;
+                }
+        }
+
+        // add contact method
         public static void AddPersonInfo()
         {
             Person persn = new Person();
@@ -37,28 +77,80 @@ namespace AddressBookProgram
             persn.EmailId = Console.ReadLine();
 
             ContactList.Add(persn);
+            DisplayContacts();
+            Operations();
         }
 
-        public static void DisplayData(Person persn)
+        //edit contact method
+        public static void ModifyPersonInfo()
         {
-            Console.WriteLine(" First Name \t: {0} \n Last Name \t: {1} ", persn.FirstName,persn.LastName);
-            Console.WriteLine(" Address \t: {0} \n City \t\t: {1} ", persn.Address, persn.City);
-            Console.WriteLine(" State \t\t: {0} \n ZipCode \t: {1} ", persn.State, persn.ZipCode);
-            Console.WriteLine(" Phone Number \t: {0} \n EmailId \t: {1} \n", persn.PhoneNumber, persn.EmailId);
+            Console.WriteLine("\n [ EDIT CONTACT ] Select Field to edit -\n 1:First_name  2:Last_name  3:Address  4:City  5:State  6.Zipcode  7:Phone_Number  8.EmailId ");
+            Console.WriteLine(" Type 0 to Exit Edit option. ");
+            Console.Write(" Please provide an option : ");
+            int choice = int.Parse(Console.ReadLine());
+
+            foreach (var persn in ContactList)
+            {
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write(" Modify FirstName : ");
+                        persn.FirstName = Console.ReadLine();
+                        return;
+                    case 2:
+                        Console.Write(" Modify LastName : ");
+                        persn.LastName = Console.ReadLine();
+                        return;
+                    case 3:
+                        Console.Write(" Modify AddressLine : ");
+                        persn.Address = Console.ReadLine();
+                        return;
+                    case 4:
+                        Console.Write(" Modify City : ");
+                        persn.City = Console.ReadLine();
+                        return;
+                    case 5:
+                        Console.Write(" Modify State : ");
+                        persn.State = Console.ReadLine();
+                        return;
+                    case 6:
+                        Console.Write(" Modify ZipCode : ");
+                        persn.ZipCode =int.Parse(Console.ReadLine());
+                        return;
+                    case 7:
+                        Console.Write(" Modify PhoneNumber : ");
+                        persn.PhoneNumber = Console.ReadLine();
+                        return;
+                    case 8:
+                        Console.Write(" Modify EmailId : ");
+                        persn.EmailId = Console.ReadLine();
+                        return;
+                    case 0:
+                        break;
+                    default:
+                        Console.WriteLine(" Invalid value entered.");
+                        break;
+                }
+            }
         }
 
-        public void Book()
+        //display saved Contacts
+        public static void DisplayContacts()
         {
-            Console.WriteLine(" Welcome to Address Book Program \n");
-
-            Console.WriteLine(" [ ADDING CONTACT ] Please provide following details : \n");
-            AddPersonInfo();
+            static void DisplayFormat(Person persn)
+            {
+                Console.WriteLine(" First Name \t: {0} \n Last Name \t: {1} ", persn.FirstName, persn.LastName);
+                Console.WriteLine(" Address \t: {0} \n City \t\t: {1} ", persn.Address, persn.City);
+                Console.WriteLine(" State \t\t: {0} \n ZipCode \t: {1} ", persn.State, persn.ZipCode);
+                Console.WriteLine(" Phone Number \t: {0} \n EmailId \t: {1} \n", persn.PhoneNumber, persn.EmailId);
+            }
 
             Console.WriteLine("\n Displying saved contact details : \n");
             foreach (var persn in ContactList)
             {
-                DisplayData(persn);
+                DisplayFormat(persn);
             }
         }
     }
 }
+
