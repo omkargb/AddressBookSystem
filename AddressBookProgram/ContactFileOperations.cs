8f5c9724d873ca880652ae7cd8ed86bc52d4d53c
omkargb <omkargb107@gmail.com>
2021-08-24 01:23:37 +0530

    [omkargb] Add : Writing contacts to and reading contacts from text file - implemented.

---
+++ AddressBookProgram/ContactFileOperations.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AddressBookProgram
+{
+    class ContactFileOperations
+    { 
+        public static void FileOps()
+        {
+            string path = @"G:\BRIDGELABZ\AddressBookSystem\AddressBookProgram\ContactFile.txt";
+            WriteDataToFile(path);
+            ReadDataFromFile(path);
+        }
+
+        static void WriteDataToFile(string path)
+        {
+            Console.WriteLine("\n - - - Writing contacts to file - - - ");
+            if (File.Exists(path))
+                {
+                    Console.WriteLine(" File exists.");
+                    StreamWriter writer = new StreamWriter(path);
+                
+                writer.WriteLine("FirstName,LastName,Address,City,State,ZipCode,PhoneNumber,EmailId");
+                foreach (var ab in AddressBookMain.contactsDictionary)
+                    {
+                        writer.WriteLine("Inside Address Book : " + ab.Key);
+                        foreach (Person person in AddressBookMain.contactsDictionary[ab.Key])
+                        {
+                            string data =
+                                person.FirstName + "," + person.LastName + "," + person.Address + "," + person.City + "," + person.State + "," + person.ZipCode + "," + person.PhoneNumber + "," + person.EmailId;
+
+                            writer.WriteLine(data);
+                        }
+                    }
+                Console.WriteLine(" File Write success.");
+                    writer.Close();
+                }
+            else            
+            {
+                    Console.WriteLine(" Process Not completed.");
+            }
+        }
+
+        static void ReadDataFromFile(string path)
+        {
+            Console.WriteLine("\n - - - Reading contacts from text file - - - ");
+            StreamReader file = new StreamReader(path);
+            using(file)
+            {
+                string data;
+                while ((data = file.ReadLine()) != null)
+                {
+                    Console.WriteLine(data);
+                }
+            }
+        }
+    }
+}
--- AddressBookProgram/ContactManager.cs
+++ AddressBookProgram/ContactManager.cs
@@ -9,7 +9,7 @@ namespace AddressBookProgram
         //options to select operation
         public static void Operations()
         {
-            Console.WriteLine("\n Available options :\n 1.Add_contact \t\t 2.Edit_contact \t 3.Delete_Contact \t 4.View_contacts \n 5.New_address_book \t\t 6.Search_person_by_cityOrState \n 7.ViewPerson_ByCityOrState \t 7.GetCount_Ofperson_byCityOrState \t 8.Sort_addressBook_contacts \n 0.Exit \n");
+            Console.WriteLine("\n Available options :\n 1.Add_contact \t\t 2.Edit_contact \t 3.Delete_Contact \t 4.View_contacts \n 5.New_address_book \t\t 6.Search_person_by_cityOrState \n 7.ViewPerson_ByCityOrState \t\t 7.GetCount_Ofperson_byCityOrState \t\t 8.Sort_addressBook_contacts \n 9.Read_Write_Contacts_UsingFileOperations \t\t 0.Exit \n");
 
             Console.Write(" Provide option :  ");
 
@@ -78,6 +78,11 @@ namespace AddressBookProgram
 
                 case 8:
                     AddressBookMain.SortAddressBook();
+                    Operations();
+                    break;
+
+                case 9:
+                    ContactFileOperations.FileOps();
                     Operations();
                     break;
 
